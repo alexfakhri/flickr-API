0260c134e26948b507afbd1ae28d1959c8a81c82
Alex Fakhri <alexfakhri@me.com>
2016-09-26 11:26:05 +0100

    Pulling in data from Flickr and using handlebars to render them on screen

--- js/interface.js
+++ js/interface.js
@@ -1,28 +1,3 @@
-// $(document).ready(function() {
-//   $('button').click(function() {
-//     $("button").removeClass("selected");
-//     $(this).addClass("selected");
-//     var flickrAPI = 'https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?';
-//     var animal = $(this).text();
-//     var flickrOptions = {
-//                         tags: animal,
-//                         format: 'json'
-//                         };
-//     function displayPhotos(data) {
-//       var photoHTML = '<ul>';
-//       $.each( data.items, function(i, photo) {
-//         photoHTML += '<li class="grid-25 tablet-grid-50">';
-//         photoHTML += '<a href="'+ photo.link +'" class="image">';
-//         photoHTML += '<img src="' + photo.media.m + '"></a></li>'
-//       });
-//       photoHTML += '</ul>';
-//       $('#photos').html(photoHTML);
-//       console.log(data)
-//     };
-//     $.getJSON(flickrAPI, flickrOptions, displayPhotos);
-//   });
-// });
-
 $(document).ready(function(){
 
   $("#flickrSearch").click(function(event){
@@ -33,10 +8,16 @@ $(document).ready(function(){
                       tags: searchTerm,
                       format: 'json'
                       };
+    var html = $('#photoTemplate').html()
     var template = Handlebars.compile(html)
-    $.getJSON(flickrAPI, apiOptions, function(photos) {
-      console.log(photos);
-    });
+      function displayPhotos(data) {
+        $.each( data.items, function(i, photos){
+          $('#photoFeed').append(template(photos));
+          console.log(data);
+
+        });
+      };
+      $.getJSON(flickrAPI, apiOptions, displayPhotos);
   });
 
 });
